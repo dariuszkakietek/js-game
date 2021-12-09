7fff93beb033e73fb94dbcc56b800d9ee55720a0
Darek <dariuszkakietek@gmail.com>
2021-12-09 18:30:41 +0100

    add phrases

---
+++ assets/js/phrases.js
@@ -0,0 +1,166 @@
+let phrases = [
+    {
+        "category": "Film",
+        "slogan": "Skazani na Shawshank"
+    },
+    {
+        "category": "Film",
+        "slogan": "Nietykalni"
+    },
+    {
+        "category": "Film",
+        "slogan": "Zielona mila"
+    },
+    {
+        "category": "Film",
+        "slogan": "Ojciec chrzestny"
+    },
+    {
+        "category": "Film",
+        "slogan": "Dwunastu gniewnych ludzi"
+    },
+    {
+        "category": "Film",
+        "slogan": "Forrest Gump"
+    },
+    {
+        "category": "Film",
+        "slogan": "Lot nad kukułczym gniazdem"
+    },
+    {
+        "category": "Film",
+        "slogan": "Ojciec chrzestny II"
+    },
+    {
+        "category": "Film",
+        "slogan": "Lista Schindlera"
+    },
+    {
+        "category": "Film",
+        "slogan": "Król Lew"
+    },
+    {
+        "category": "Film",
+        "slogan": "Więzień nienawiści"
+    },
+    {
+        "category": "Film",
+        "slogan": "Chłopiec w pasiastej piżamie"
+    },
+    {
+        "category": "Film",
+        "slogan": "W pogoni za szczęściem"
+    },
+    {
+        "category": "Serial",
+        "slogan": "Czarnobyl"
+    },
+    {
+        "category": "Serial",
+        "slogan": "Breaking Bad"
+    },
+    {
+        "category": "Serial",
+        "slogan": "Rick i Morty"
+    },
+    {
+        "category": "Serial",
+        "slogan": "Kompania braci"
+    },
+    {
+        "category": "Serial",
+        "slogan": "Dom z papieru"
+    },
+    {
+        "category": "Serial",
+        "slogan": "House of Cards"
+    },
+    {
+        "category": "Serial",
+        "slogan": "Miasteczko Twin Peaks"
+    },
+    {
+        "category": "Serial",
+        "slogan": "Stranger Things"
+    },
+    {
+        "category": "Serial",
+        "slogan": "Czarne lustro"
+    },
+    {
+        "category": "Serial",
+        "slogan": "Notatnik śmierci"
+    },
+    {
+        "category": "Serial",
+        "slogan": "Rodzina Soprano"
+    },
+    {
+        "category": "Przysłowie",
+        "slogan": "Bez pracy nie ma kołaczy"
+    },
+    {
+        "category": "Przysłowie",
+        "slogan": "Darowanemu koniowi w zęby się nie zagląda"
+    },
+    {
+        "category": "Przysłowie",
+        "slogan": "Fortuna kołem się toczy"
+    },
+    {
+        "category": "Przysłowie",
+        "slogan": "Nie chwal dnia przed zachodem słońca"
+    },
+    {
+        "category": "Przysłowie",
+        "slogan": "Lepszy wróbel w garści niż gołąb na dachu"
+    },
+    {
+        "category": "Przysłowie",
+        "slogan": "Apetyt rośnie w miarę jedzenia"
+    },
+    {
+        "category": "Przysłowie",
+        "slogan": "Co ma wisieć nie utonie"
+    },
+    {
+        "category": "Przysłowie",
+        "slogan": "Dzieci i ryby głosu nie mają"
+    },
+    {
+        "category": "Przysłowie",
+        "slogan": "Grosz do grosza a będzie kokosza"
+    },
+    {
+        "category": "Przysłowie",
+        "slogan": "Łaska pańska na pstrym koniu jeździ"
+    },
+    {
+        "category": "Słynne cytaty z polskich komedii",
+        "slogan": "Bunkrów nie ma ale też jest zajebiście"
+    },
+    {
+        "category": "Słynne cytaty z polskich komedii",
+        "slogan": "Co Ty kurwa wiesz o zabijaniu"
+    },
+    {
+        "category": "Słynne cytaty z polskich komedii",
+        "slogan": "Wpuścić chamstwo na salony"
+    },
+    {
+        "category": "Słynne cytaty z polskich komedii",
+        "slogan": "Kaczka to max co może z ciebie być"
+    },
+    {
+        "category": "Słynne cytaty z polskich komedii",
+        "slogan": "Ciemność ja ciemność widzę"
+    },
+    {
+        "category": "Słynne cytaty z polskich komedii",
+        "slogan": "Czysta wódka ani honoru ani munduru nie plami"
+    },
+    {
+        "category": "Słynne cytaty z polskich komedii",
+        "slogan": "koko jumbo i do przodu"
+    }
+]
